928772bf3117df3dd95966cb553ae28f03ccee7a
Simar Chadha <simarchadha1@gmail.com>
2023-12-13 17:30:55 -0500

    User fixes

--- src/Basketboxd/Search/index.js
+++ src/Basketboxd/Search/index.js
@@ -3,6 +3,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import getAllPlayers from "./getAllPlayers";
 import teamColors from "../teamColors";
+import { RotatingSquare } from 'react-loader-spinner'
 
 function Search() {
   const NBA_STATS_API = "https://nba-stats-db.herokuapp.com/api";
@@ -10,12 +11,14 @@ function Search() {
   const playerName = searchParams.get("player");
   const year = searchParams.get("year");
   const [players, setPlayers] = useState([]);
+  const [queryLoaded, setQueryLoaded] = useState(false);
   const playersBySeasonURL = `${NBA_STATS_API}/playerdata/season/${year}`;
 
   const navigate = useNavigate();
 
   const fetchPlayers = async () => {
     try {
+      setQueryLoaded(false);
       setPlayers([]);
 
       const listOfPlayers = await getAllPlayers(year, 1);
@@ -25,7 +28,7 @@ function Search() {
       const sortedPlayers = filteredPlayers.slice().sort((a, b) => {
         const nameA = a.player_name.toLowerCase();
         const nameB = b.player_name.toLowerCase();
-      
+
         if (nameA < nameB) {
           return -1;
         }
@@ -34,6 +37,8 @@ function Search() {
         }
         return 0;
       });
+      setQueryLoaded(true);
+      console.log("Ending with: " + queryLoaded)
       setPlayers(sortedPlayers);
     } catch (error) {
       console.error("Error fetching players:", error);
@@ -51,16 +56,27 @@ function Search() {
   return (
     <div>
       <h1>Results for players in {year}: {playerName}</h1>
+      <div className="row justify-content-center">
+        <RotatingSquare
+          height="100"
+          width="100"
+          color="#ffffff"
+          ariaLabel="rotating-square-loading"
+          wrapperClass="justify-content-center"
+          strokeWidth="4"
+          visible={!queryLoaded}
+        />
+      </div>
 
-      <div className="d-flex flex-row flex-wrap mb-4" style={{ marginLeft: '30px', marginRight: '30px'}}>
+      <div className="d-flex flex-row flex-wrap mb-4" style={{ marginLeft: '30px', marginRight: '30px' }}>
         {players.map((player) => (
           <div className="card" key={player.player_name + player.team} onClick={() => handlePlayerClick(player.player_name)}
-          style={{ backgroundColor: teamColors(player.team).background, color: teamColors(player.team).text}}>
+            style={{ backgroundColor: teamColors(player.team).background, color: teamColors(player.team).text }}>
             <div className="card-body">
               <p className="card-text">
                 <Link
                   to={`/Players/${encodeURIComponent(player.player_name)}`}
-                  style={{color: teamColors(player.team).text}}
+                  style={{ color: teamColors(player.team).text }}
                 >
                   {player.player_name}
                 </Link>
--- src/Basketboxd/users/table.js
+++ src/Basketboxd/users/table.js
@@ -7,6 +7,7 @@ import {
   BsPencil,
   BsPlusCircleFill,
 } from "react-icons/bs";
+import { useSelector } from "react-redux";
 function UserTable() {
   const [users, setUsers] = useState([]);
   const fetchUsers = async () => {
@@ -17,7 +18,13 @@ function UserTable() {
     username: "",
     password: "",
     role: "USER",
+    firstName: "",
+    lastName: "",
   });
+  const resetUser = () => {
+    setUser({ username: "", password: "", role: "USER", firstName: "", lastName: "" });
+  }
+  const { currentUser } = useSelector((state) => state.userReducer);
   const createUser = async () => {
     try {
       const newUser = await client.createUser(user);
@@ -55,82 +62,105 @@ function UserTable() {
   }, []);
   return (
     <div>
-      <h1>User List</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Username</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-          </tr>
-          <tr>
-            <td>
-              <input
-                value={user.username}
-                onChange={(e) => setUser({ ...user, username: e.target.value })}
-              />
-              <input
-                value={user.password}
-                onChange={(e) => setUser({ ...user, password: e.target.value })}
-              />
-            </td>
-            <td>
-              <input
-                value={user.firstName}
-                onChange={(e) =>
-                  setUser({ ...user, firstName: e.target.value })
-                }
-              />
-            </td>
-            <td>
-              <input
-                value={user.lastName}
-                onChange={(e) => setUser({ ...user, lastName: e.target.value })}
-              />
-            </td>
-            <td>
-              <select
-                value={user.role}
-                onChange={(e) => setUser({ ...user, role: e.target.value })}
-              >
-                <option value="USER">User</option>
-                <option value="ADMIN">Admin</option>
-              </select>
-            </td>
-            <td>
-              <BsPlusCircleFill
-                onClick={createUser}
-                className="text-primary fs-1 text"
-              />
-              <BsFillCheckCircleFill
-                onClick={updateUser}
-                className="me-2 text-success fs-1 text"
-              />
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td>
-              <Link to={`/Basketboxd/account/${user._id}`}>
-                {user.username}
-              </Link>
-              </td>
-              <td>{user.firstName}</td>
-              <td>{user.lastName}</td>
-              <td>
-                <button className="btn btn-warning me-2">
-                  <BsPencil onClick={() => selectUser(user)} />
-                </button>
-                <button className="btn btn-danger me-2">
-                  <BsTrash3Fill onClick={() => deleteUser(user)} />
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {currentUser !== null && currentUser.role === "ADMIN" &&
+        <>
+          <h1>User List</h1>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Username</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Role</th>
+                <th>Actions</th>
+              </tr>
+              <tr>
+                <td>
+                  <input
+                    value={user.username}
+                    placeholder="Username"
+                    onChange={(e) => setUser({ ...user, username: e.target.value })}
+                  />
+                </td>
+                <td>
+                  <input
+                    value={user.firstName}
+                    placeholder="First Name"
+                    onChange={(e) =>
+                      setUser({ ...user, firstName: e.target.value })
+                    }
+                  />
+                </td>
+                <td>
+                  <input
+                    value={user.lastName}
+                    placeholder="Last Name"
+                    onChange={(e) => setUser({ ...user, lastName: e.target.value })}
+                  />
+                </td>
+                <td>
+                  <select
+                    value={user.role}
+                    onChange={(e) => setUser({ ...user, role: e.target.value })}
+                  >
+                    <option value="USER">User</option>
+                    <option value="ADMIN">Admin</option>
+                  </select>
+                </td>
+                <td>
+                  <button className="btn bg-none me-2">
+                    <BsPlusCircleFill
+                      onClick={createUser}
+                      title="Create User"
+                      className="text-primary fs-1 text"
+                    />
+                  </button>
+                  <button className="btn bg-none me-2">
+                    <BsFillCheckCircleFill
+                      onClick={updateUser}
+                      title="Save User"
+                      className="me-2 text-success fs-1 text"
+                    />
+                  </button>
+                  <button className="btn bg-none me-2">
+                    <BsTrash3Fill
+                      className="text-danger fs-1 text"
+                      onClick={resetUser} />
+                  </button>
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>
+                    <Link to={`/account/${user.username}`}>
+                      {user.username}
+                    </Link>
+                  </td>
+                  <td>{user.firstName}</td>
+                  <td>{user.lastName}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <button className="btn btn-warning me-2">
+                      <BsPencil onClick={() => selectUser(user)} />
+                    </button>
+                    <button className="btn btn-danger me-2">
+                      <BsTrash3Fill onClick={() => deleteUser(user)} />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      }
+      {currentUser?.role !== "ADMIN" &&
+        <>
+          <h1 className="text-danger">Access Denied</h1>
+          <h3 className="text-danger">You must be an admin to view this page</h3>
+        </>
+      }
     </div>
   );
 }
