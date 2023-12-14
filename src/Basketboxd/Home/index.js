b671820e988630aa406504672a457f7150b013bd
rrau01 <rau.r@northeastern.edu>
2023-12-13 21:33:02 -0500

    added welcome message

--- src/Basketboxd/Home/index.js
+++ src/Basketboxd/Home/index.js
@@ -1,9 +1,21 @@
 import TopPlayers from "./TopPlayers";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
 
 function Home() {
 
+  const [account, setAccount] = useState(null);
+  const { currentUser } = useSelector((state) => state.userReducer);
+  useEffect(() => {
+    setAccount(currentUser);
+  }, [currentUser]);
+
   return (
     <div>
+        (account !== null && (
+          <h3>Welcome {currentUser.username}!</h3>
+        ))
         <h1>Top Players for 2022-2023</h1>
         <TopPlayers />
     </div>
